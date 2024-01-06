a4c37d9bb5af1698f3867fdc318fd6a6c3e9093d
Santhosh R <santhoshr@live.com>
2024-01-07 00:52:29 +0530

    Added WezTerm config file

---
+++ dotfiles/wezterm/.wezterm.lua
@@ -0,0 +1,14 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.font = wezterm.font 'Monaco'
+config.font_size = 16
+config.color_scheme = 'Adventure Time (Gogh)'
+
+return config
